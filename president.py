1f3864eae346ec440f98e401756e3ea400f29738
Tom Denham <tom.denham@metaswitch.com>
2015-12-04 21:33:20 -0800

    Add support for forced consecutive play
    Moves are now all lists of cards (since players can put down more than one card)

--- president.py
+++ president.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 from copy import deepcopy
 import random
+import sys
 from framework import GameState, Card, ismcts, Deck
+
+
+# TODO:
+# Make it faster? https://cardsource.readthedocs.org/en/latest/
+# Make it look better -https://github.com/worldveil/deuces
+# Put a UI on it
+# Let it play int he real world - http://arnab.org/blog/so-i-suck-24-automating-card-games-using-opencv-and-python
+#                               - https://rdmilligan.wordpress.com/2014/08/30/playing-card-detection-using-opencv-mark-v/
+# Make two the high card
+# Allow it to work with any number of players (pass handling become more complicated)
+#
+CONSECUTIVE = "CONSECUTIVE"
+RUN = "RUN"
+QUADS = "QUADS"
+TRIPS = "TRIPS"
+DUBS = "DUBS"
 
 
 class PresidentGameState(GameState):
@@ -21,10 +38,12 @@ class PresidentGameState(GameState):
 
         """
         GameState.__init__(self)
+        self.player_to_move = 0
         self.player_hands = [[],[]]
         self.discards = []
         self.current_trick = []
-        self.player_to_move = 0
+        # Modes can be CONSECUTIVE, DUBS, TRIPS, QUADS, RUN
+        self.modes = set()
 
         self._deal()
 
@@ -36,6 +55,7 @@ class PresidentGameState(GameState):
         st.player_hands = deepcopy(self.player_hands)
         st.discards = deepcopy(self.discards)
         st.current_trick = deepcopy(self.current_trick)
+        st.modes = deepcopy(self.modes)
         return st
 
     def clone_and_randomize(self, observer):
@@ -46,22 +66,24 @@ class PresidentGameState(GameState):
 
         # The observer can see his own hand and the cards in the current trick,
         # and can remember the cards played in previous tricks
-        seen_cards = st.player_hands[observer] + st.discards + st.current_trick
+        flattened_current_trick =  [a for b in st.current_trick for a in b]
+        seen_cards = st.player_hands[observer] + st.discards + flattened_current_trick
 
         # The observer can't see the rest of the deck
         unseen_cards = [card for card in Deck().cards
                         if card not in seen_cards]
 
-        # _deal the unseen cards to the other players
+        # deal the unseen cards to the player
         random.shuffle(unseen_cards)
+
         for p in (0,1):
             if p != observer:
                 # deal cards to player p. The players start with 17 cards, so
                 # with two players there are 34 cards in total.
-                # The play should be dealt 34 - discards - num cards in other players hand.
+                # The player should be dealt 34 - discards - num cards in other players hand.
 
                 # Store the size of player p's hand
-                num_cards = 34 - len(self.discards) - len(self.player_hands[self.get_next_player(p)])
+                num_cards = 34 - len(self.discards) - len(self.player_hands[observer])
 
                 # Give player p the first num_cards unseen cards
                 st.player_hands[p] = unseen_cards[:num_cards]
@@ -90,16 +112,46 @@ class PresidentGameState(GameState):
             Must update player_to_move.
         """
         # If the move is "PASS" then the current trick is over
-        if move is "PASS":
+        if "PASS" in move:
             # Trick over so update the game state
-            self.discards.extend(self.current_trick)
+            self.discards.extend([a for b in self.current_trick for a in b])
             self.current_trick = []
+            self.modes.clear()
         else:
+            if len(move) > 1:
+                # The player put down multiple cards - add modes
+                if move[0].rank == move[1].rank:
+                    # Not a sequence
+                    if len(move) == 2:
+                        self.modes.add(DUBS)
+                    elif len(move) == 3:
+                        self.modes.add(TRIPS)
+                    elif len(move) == 2:
+                        self.modes.add(QUADS)
+                else:
+                    # a sequence
+                    self.modes.add(RUN)
+
+            # On the second move, check for CONSECUTIVE mode
+            if len(self.current_trick) == 1:
+                if DUBS in self.modes or TRIPS in self.modes or QUADS in self.modes \
+                    and self.current_trick[0][0].rank + 1 == move.rank[0]:
+                    # Just need to check one of the cards in the first hand
+                    self.modes.add(CONSECUTIVE)
+                elif RUN in self.modes and self.current_trick[0][-1].rank +1 == move.rank[0][0]:
+                    # Check the highest card (assume they are ordered) on the first hand.
+                    self.modes.add(CONSECUTIVE)
+                elif self.current_trick[0][0].rank + 1 == move[0].rank:
+                    # print "First card: %s Second Card: %s" % (self.current_trick, move)
+                    self.modes.add(CONSECUTIVE)
+                    # print self.modes
+
             # Store the played card in the current trick
             self.current_trick.append(move)
 
             # Remove the card from the player's hand
-            self.player_hands[self.player_to_move].remove(move)
+            for card in move:
+                self.player_hands[self.player_to_move].remove(card)
 
         # Find the next player - always change players
         self.player_to_move = self.get_next_player(self.player_to_move)
@@ -110,21 +162,28 @@ class PresidentGameState(GameState):
         Get all possible moves from this state.
         """
         hand = self.player_hands[self.player_to_move]
+        if not hand:
+            # If there are no moves left, then return the empty list.
+            return hand
         if not self.current_trick:
             # May lead a trick with any card. Can't pass - that would be silly.
-            return hand
-        elif not hand:
-            # If there are no moves left, then return the empty list.
-            # Don't allow pass, that would be silly.
-            return hand
+            # Moves may involve multiple cards, so return a list of lists.
+            return [[card] for card in hand]
         else:
+            # Start by picking out just the higher cards. Card rank needs to be strictly greater.
+            # Grab the rank of the last card from the last trick.
+            last_card_rank = self.current_trick[-1][-1].rank
+            if CONSECUTIVE in self.modes:
+                moves = [[card] for card in hand if card.rank == last_card_rank + 1]
+            else:
+                moves = [[card] for card in hand if card.rank > last_card_rank]
+
             # If started with a multi card then need to follow
             # If started with a sequence then need to follow
             # If started in consecutive mode then need to follow
 
-            # Card rank needs to be strictly greater. And can always pass.
-            last_card = self.current_trick[-1]
-            return [card for card in hand if card.rank > last_card.rank] + ["PASS"]
+            # Can always pass.
+            return  moves + [["PASS"]]
 
     def get_result(self, player):
         """
@@ -136,11 +195,11 @@ class PresidentGameState(GameState):
     def __repr__(self):
         """ Return a human-readable representation of the state
         """
-        result = "P%i: " % self.player_to_move
-        result += ",".join(
-            str(card) for card in self.player_hands[self.player_to_move])
-        result += " | Trick: "
-        result += str(self.current_trick)
+        result = "P%i: %s Mode: %s Trick: %s Discards: %s" % (self.player_to_move,
+                                                     self.player_hands[self.player_to_move],
+                                                     self.modes,
+                                                     self.current_trick,
+                                                     self.discards)
         return result
 
 
